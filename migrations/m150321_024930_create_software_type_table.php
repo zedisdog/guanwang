1d49b00a07ba4a84b44c4596b8701b46bd85f46f
zed <zedsmail@163.com>
2015-03-21 11:54:31 +0800

    --

--- migrations/m150321_024930_create_software_type_table.php
+++ migrations/m150321_024930_create_software_type_table.php
@@ -3,20 +3,23 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m150321_024930_create_software_type_table extends Migration
-{
-    public function up()
-    {
-
+class m150321_024930_create_software_type_table extends Migration{
+    public function up(){
+        $this->createTable('software_type', [
+            'id' => 'pk',
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'create_time' => Schema::TYPE_DATETIME.' NOT NULL',
+            'update_time' => Schema::TYPE_DATETIME.' NOT NULL',
+        ]);
     }
 
-    public function down()
-    {
-        echo "m150321_024930_create_software_type_table cannot be reverted.\n";
+    public function down(){
+        $this->dropTable('software_type');
+        echo "drop table m150321_024930_create_software_type_table.\n";
 
-        return false;
+        return true;
     }
-    
+
     /*
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
