9907e9baaecbd0e9a9ee6e4cf98cf020922a532f
zedisdog <zedsmail@163.com>
2015-03-25 10:25:25 +0800

    kindeditor 文件上传路径

--- modules/admin/views/article/edit_article.php
+++ modules/admin/views/article/edit_article.php
@@ -1,8 +1,33 @@
 <?php
 use app\assets\admin\EditAsset;
+use yii\widgets\ActiveForm;
 
 EditAsset::register($this);
 ?>
 <section class="content">
-<textarea id="content"></textarea>
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header">
+                <?php echo $item->id?'编辑':'添加' ?>文章
+            </div>
+            <div class="box-body">
+                <form action="/admin/article/edit" method="post">
+                    <input type="hidden" value="" />
+                    <div class="form-group">
+                        <label>标题</label>
+                        <input name="title" type="text" class="form-control" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label>来源</label>
+                        <input name="source" type="text" class="form-control" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label>来源URL</label>
+                        <input name="source_url" type="text" class="form-control" placeholder="">
+                    </div>
+                    <textarea id="content"></textarea>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
--- web/admin/js/edit.js
+++ web/admin/js/edit.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var editor;
     KindEditor.ready(function(K){
         editor = K.create('#content',{
-
+            width : '100%'
         });
     });
 });
